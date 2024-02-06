387e0a21afca9186d680885dc0091d452e473206
Sung Ji <42290597+jaducku@users.noreply.github.com>
2024-02-06 19:51:41 +0900

    클래스형태로 변경 및 프롬프트 숨김처리

--- app.py
+++ app.py
@@ -1,36 +1,58 @@
 import discord
 from discord.ext import commands
-
 import os
 from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-from transformers import AutoProcessor, SeamlessM4Tv2Model 
+class DiscordBot:
+    def __init__(self, bot_key, open_api_key, chat_model, prompt_template):
+        self.bot_key = bot_key
+        self.open_api_key = open_api_key
+        self.chat_model = chat_model
+        self.prompt_template = prompt_template
 
-processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large") 
-model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
+        intents = discord.Intents.default()
+        intents.message_content = True
+        self.bot = commands.Bot(command_prefix='!', intents=intents)
 
-load_dotenv()
-bot_key = os.getenv("BOT_KEY")
+        self.setup()
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+    def setup(self):
+        @self.bot.event
+        async def on_ready():
+            print("Bot Is Ready And Online!")
 
-@bot.event
-async def on_ready():
-    print("Bot Is Ready And Online!")
+        @self.bot.event
+        async def on_message(message):
+            if message.author.bot:
+                return
 
-@bot.event
-async def on_message(message):
-    if message.author.bot == False:
-        if message.channel.name == 'global':
-            text_inputs = processor(text=message.content, src_lang="kor", return_tensors="pt")
-            output_tokens = model.generate(**text_inputs, tgt_lang="eng", generate_speech=False) 
-            interpreted_message = processor.decode(output_tokens[0].tolist()[0], skip_special_tokens=True)
+            if message.channel.name == 'global':
+                input_text = message.content
+                res = self.invoke_translation_chain(input_text)
+                await message.reply(res)
+                
+            await self.bot.process_commands(message)
 
-            await message.channel.send(interpreted_message,reference=message)
-    else:
-        return
-    await bot.process_commands(message)
+    def invoke_translation_chain(self, input_text):
+        prompt = ChatPromptTemplate.from_template(self.prompt_template + " {input_text}")
+        model = ChatOpenAI(openai_api_key=self.open_api_key, model=self.chat_model)
+        output_parser = StrOutputParser()
 
-bot.run(bot_key)
+        chain = prompt | model | output_parser
+        return chain.invoke({"input_text": input_text})
+
+    def run(self):
+        self.bot.run(self.bot_key)
+
+if __name__ == "__main__":
+    load_dotenv()
+    bot_key = os.getenv("BOT_KEY")
+    open_api_key = os.getenv("OPENAI_API_KEY")
+    chat_model = os.getenv("CHAT_MODEL")
+    prompt_template = os.getenv("PROMPT_TEMPLATE")
+
+    bot = DiscordBot(bot_key, open_api_key, chat_model, prompt_template)
+    bot.run()
